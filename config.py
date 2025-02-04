6015e7f3e9caaa1acf383795a5e59434fd7c7334
Karma Woeser <karmawoeser@gmail.com>
2025-02-04 12:00:05 -0800

    added mongodb config

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 class Config:
     """Base configuration class."""
     SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/easya")
-
+    # MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/easya")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://kwoeser:<db_password>@easya.brvnj.mongodb.net/?retryWrites=true&w=majority&appName=EasyA")
+    
 
 """
 from dotenv import load_dotenv
